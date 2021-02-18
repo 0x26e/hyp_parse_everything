87d0dbe2da681dd66458db5fd41300bb8baf0cfb
0x26e <78525013+0x26e@users.noreply.github.com>
2021-02-17 20:27:37 -0500

    Create constants.py

---
+++ sections/Quakecraft/constants.py
@@ -0,0 +1,40 @@
+# File for all Quakecraft related constants
+
+# List of active stats
+quake_active_stats = {"barrel": "barrel", "case": "case", "killsound": "killsound", "sight": "laser",
+    "muzzle": "muzzle", "beam": "beam", "selectedKillPrefix": "prefix_color", "trigger": "trigger"
+    }
+
+# List of gamemodes
+quake_gamemodes = {"_teams": "teams", "_solo_tourney": "tournament", "": "solo"}
+
+# Setup general stat names
+quake_stat_names = (
+    "coins", "highest_killstreak", "dash_cooldown", "dash_power", "weekly_kills_a", "weekly_kills_b", "monthly_kills_a", "monthly_kills_b"
+    )
+
+# Setup mode stats
+quake_mode_stats = ("kills", "deaths", "killstreaks", "wins", "headshots", "distance_travelled", "shots_fired")
+
+# Setup for armor
+quake_hats = (
+    ("exodiahat", 200000, "None"), ("discohat", 75000, "None"), ("radianthat", 50000, "None"), ("bouncyhat", 25000, "None"),
+    ("crafterhat", 15000, "None"), ("explosivehat", 5000, "None"), ("librarianhat", 2500, "None"), ("hypixelhat", 2000, "None"),
+    ("bobhat", 1500, "None"), ("ecologyhat", 1000, "None"), ("lighthat", 1000, "None"), ("engineeringhat", 1000, "None"),
+    ("richyrichhat", 1000, "None"), ("jeeperscreepershat", 900, "None"), ("walkingdeadhat", 900, "None"), ("controlfreakhat", 850, "None"),
+    ("cavemanhat", 850, "None"), ("hipsterhat", 850, "None"), ("majestichat", 700, "None"), ("tnt", 700, "None"), ("dispenser", 700, "None"),
+    ("diamond", 700, "None"), ("redstone", 700, "None"), ("lantern", 700, "None"), ("spaceman", 700, "None"), ("melon", 350, "None"),
+    ("cactus", 350, "None"), ("showoff", 0, "None")
+    )
+quake_chestplates = (
+    ("exodiakit", 200000, "None"), ("discokit", 75000, "None"), ("swegkit", 4500, "None"), ("budderkit", 3375, "None"), ("medievalkit", 2750, "None"),
+    ("fashionistakit", 2250, "None"), ("snowkit", 2250, "None"), ("slimekit", 2250, "None"), ("spacekit", 2250, "None"), ("revengekit", 2250, "None"),
+    ("invaderkit", 2250, "None"), ("specopskit", 2250, "None"), ("swatkit", 2250, "None"), ("marinekit", 2250, "None"), ("commander", 2250, "None"),
+    ("majestic", 2250, "None"), ("elite", 2250, "None"), ("soldier", 650, "None")
+    )
+quake_lowers = (
+    ("exodia", 200000, "None"), ("disco", 75000, "None"), ("swegkit", 4500, "None"), ("budderkit", 3375, "None"), ("medievalkit", 2750, "None"),
+    ("fashionistakit", 2250, "None"), ("snowkit", 2250, "None"), ("slimekit", 2250, "None"), ("spacekit", 2250, "None"), ("revengekit", 2250, "None"),
+    ("invaderkit", 2250, "None"), ("specopskit", 2250, "None"), ("swatkit", 2250, "None"), ("marinekit", 2250, "None"), ("commander", 2250, "None"),
+    ("majestic", 2250, "None"), ("elite", 2250, "None"), ("soldier", 650, "None")
+    )
