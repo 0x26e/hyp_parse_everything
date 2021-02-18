5e32000f6a8d08e9d28a6da7cbceb226f275bdf4
0x26e <78525013+0x26e@users.noreply.github.com>
2021-02-18 18:04:52 -0500

    Update constants.py

--- sections/Quakecraft/constants.py
+++ sections/Quakecraft/constants.py
@@ -1,6 +1,6 @@
 # File for all Quakecraft related constants
 
-# List of active stats
+# List of active stats and name conversions
 quake_active_stats = {"barrel": "barrel", "case": "case", "killsound": "killsound", "sight": "laser",
     "muzzle": "muzzle", "beam": "beam", "selectedKillPrefix": "prefix_color", "trigger": "trigger"
     }
@@ -8,15 +8,15 @@ quake_active_stats = {"barrel": "barrel", "case": "case", "killsound": "killsoun
 # List of gamemodes
 quake_gamemodes = {"_teams": "teams", "_solo_tourney": "tournament", "": "solo"}
 
-# Setup general stat names
+# General stats with name conversions
 quake_stat_names = (
     "coins", "highest_killstreak", "dash_cooldown", "dash_power", "weekly_kills_a", "weekly_kills_b", "monthly_kills_a", "monthly_kills_b"
     )
 
-# Setup mode stats
+# General stats without name conversions
 quake_mode_stats = ("kills", "deaths", "killstreaks", "wins", "headshots", "distance_travelled", "shots_fired")
 
-# Setup for armor
+# List of cosmetic armor pieces
 quake_hats = (
     ("exodiahat", 200000, "None"), ("discohat", 75000, "None"), ("radianthat", 50000, "None"), ("bouncyhat", 25000, "None"),
     ("crafterhat", 15000, "None"), ("explosivehat", 5000, "None"), ("librarianhat", 2500, "None"), ("hypixelhat", 2000, "None"),
@@ -38,3 +38,107 @@ quake_lowers = (
     ("invaderkit", 2250, "None"), ("specopskit", 2250, "None"), ("swatkit", 2250, "None"), ("marinekit", 2250, "None"), ("commander", 2250, "None"),
     ("majestic", 2250, "None"), ("elite", 2250, "None"), ("soldier", 650, "None")
     )
+
+# List of premade weapons and their parts
+quake_weapons = {
+    "basic_railgun": ("WOOD_HOE", "YELLOW", "SMALL_BALL", "NONE", "ONE_POINT_FIVE"),
+    "superior_railgun": ("IRON_HOE", "RED", "BURST", "NONE", "ONE_POINT_FOUR"),
+    "hyper_beam_railgun": ("GOLD_HOE", "YELLOW", "STAR", "NONE", "ONE_POINT_THREE"),
+    "creeper_railgun": ("DIAMOND_HOE", "GREEN", "CREEPER", "NONE", "ONE_POINT_TWO"),
+    "bfg": ("SHINY_STONE_HOE", "BLUE", "LARGE_BALL", "NONE", "ONE_POINT_ONE"),
+    "budder_slapper": ("SHINY_GOLD_HOE", "YELLOW", "SMALL_BALL", "GOLD", "ONE_POINT_FIVE"),
+    "redstoner": ("SHINY_WOOD_HOE", "RED", "BURST", "REDSTONE", "ONE_POINT_TWO"),
+    "bling_bling_thing": ("SHINY_DIAMOND_HOE", "PURPLE", "LARGE_BALL", "DIAMOND", "ONE_POINT_ZERO"),
+    "the_turtle": ("IRON_HOE", "GREEN", "LARGE_BALL", "IRON", "TWO_POINT_FIVE"),
+    "lovegun": ("SHINY_GOLD_HOE", "RED", "STAR", "REDSTONE", "NINE_POINT_THREE"),
+    "the_harvester": ("STONE_HOE", "GREEN", "SMALL_BALL", "MELON", "ONE_POINT_FIVE"),
+    "blue_lagoon": ("DIAMOND_HOE", "BLUE", "BURST", "PRISMARINE", "ONE_POINT_ONE"),
+    "cookie_cannon": ("GOLD_HOE", "EMERALD", "BURST", "COMMAND_BLOCK", "ONE_POINT_ONE"),
+    "the_worm": ("STONE_HOE", "GREEN", "SMALL_BALL", "NONE", "FIVE_POINT_ZERO"),
+    "the_snail": ("STONE_HOE", "RED", "CREEPER", "WOOD", "NINE_POINT_ZERO"),
+    "le_bizarre": ("SHINY_DIAMOND_HOE", "ORANGE", "SMALL_BALL", "LAPIS", "ONE_POINT_FOUR"),
+    "fabled_railgun": ("SHINY_DIAMOND_HOE", "DIAMOND", "STAR", "PRISMARINE", "ONE_POINT_TWO"),
+    "apple_corer": ("SHINY_IRON_HOE", "GOLD", "STAR", "OBSIDIAN", "ZERO_POINT_NINE"),
+    "railgun_of_darkness": ("SHINY_GOLD_HOE", "BLACK", "LARGE_BALL", "OBSIDIAN", "ZERO_POINT_NINE"),
+    "platinum_smelter": ("SHINY_IRON_HOE", "SILVER", "BURST", "IRON", "ZERO_POINT_EIGHT_FIVE"),
+    "the_reaper": ("SHINY_DIAMOND_HOE", "BLACK", "LARGE_BALL", "SOUL_SAND", "ZERO_POINT_EIGHT_FIVE")
+    }
+
+# List of casing prefixes
+quake_case_prefixes = {
+    "WOOD_HOE": "Slightly",
+    "STONE_HOE": "Mostly",
+    "SHINY_WOOD_HOE": "Spectacularly",
+    "SHINY_STONE_HOE": "Brilliant",
+    "IRON_HOE": "Excellently",
+    "GOLD_HOE": "Awesomely",
+    "DIAMOND_HOE": "Undeniably",
+    "SHINY_GOLD_HOE": "Luxury",
+    "SHINY_DIAMOND_HOE": "Lucky",
+    "SHINY_IRON_HOE": "Executive"
+    }
+
+# List of laser prefixes
+quake_laser_prefixes = {
+    "YELLOW": "Shining",
+    "GREEN": "Glowing",
+    "WHITE": "Lofty",
+    "RED": "Radiant",
+    "BLUE": "Resplendant",
+    "PURPLE": "Majestic",
+    "PINK": "Stunning",
+    "GOLD": "Grandiose",
+    "EMERALD": "Dignified",
+    "DIAMOND": "Royal",
+    "GRAY": "Pearly",
+    "ORANGE": "Not-a-fruit",
+    "SILVER": "Lustrous",
+    "BLACK": "Absorbing"
+    }
+
+# List of muzzle suffixes
+quake_muzzle_suffixes = {
+    "NONE": "AK",
+    "CLAY": "CL",
+    "WOOD": "WO",
+    "GOLD": "GD",
+    "REDSTONE": "RED",
+    "IRON": "IN",
+    "ENDER_STONE": "EST",
+    "DIAMOND": "DMD",
+    "QUARTZ": "QA",
+    "EMERALD": "EM",
+    "LAPIS": "LA",
+    "OBSIDIAN": "BBY",
+    "SPONGE": "HYPE",
+    "SEA_LANTERN": "SL",
+    "COMMAND_BLOCK": "CMD",
+    "PRISMARINE": "PRM",
+    "MELON": "MEL",
+    "PUMPKIN": "PMK",
+    "SOUL_SAND": "SMS"
+    }
+
+# List of trigger suffixes
+quake_trigger_suffixes = {
+    "ONE_POINT_FIVE": "1.0",
+    "NINE_POINT_ZERO": "WTF",
+    "FIVE_POINT_ZERO": "5.0",
+    "TWO_POINT_FIVE": "2.5",
+    "ONE_POINT_FOUR": "2.0",
+    "ONE_POINT_THREE": "3.0",
+    "ONE_POINT_TWO": "1337",
+    "ONE_POINT_ONE": "2013",
+    "ONE_POINT_ZERO": "99",
+    "ZERO_POINT_NINE": "-1",
+    "ZERO_POINT_EIGHT_FIVE": "404"
+    }
+
+# List of barrel suffixes
+quake_barrel_suffixes = {
+    "SMALL_BALL": "(S)",
+    "LARGE_BALL": "(L)",
+    "CREEPER": "(C)",
+    "BURST": "(B)",
+    "STAR": "(*)"
+    }
